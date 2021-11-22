369224ae53a0bb17ea4d205a3f2a5a2709fe5aad
Sarang Bang <bangsarang7@gmail.com>
2021-11-22 10:35:58 +0530

    commit1

---
+++ index.js
@@ -0,0 +1,33 @@
+console.log("success");
+let weather = {
+    fetchweather: function (city) {
+        fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=9d2a5930e52d19db79d3ed4e3641a101&units=metric").then((response) => response.json()).then((data) => this.displayweather(data));
+    },
+    displayweather: function (data) {
+        console.log(data);
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, temp_min, temp_max } = data.main;
+        const { speed } = data.wind;
+        const { visibility }=data;
+        const temp_final = parseInt(temp, 10);
+        const visi_final = visibility/1000;
+        console.log(name);
+        console.log(visi_final);
+        document.getElementById("name").innerHTML = name;
+        document.getElementById("temp").innerHTML = temp_final + " * c";
+        document.getElementById("desp").innerText = "Description : " + description;
+        document.getElementById("speed").innerText = "Wind Speed : " + speed + " km\hr";
+        document.getElementById("visibility").innerText = "Visibility : " + visi_final + " km";
+
+    },
+    searchresult: function () {
+        this.fetchweather(document.getElementById("city").value);
+    }
+};
+
+
+function search() {
+    weather.searchresult();
+    document.getElementById("city").value = null;
+}
