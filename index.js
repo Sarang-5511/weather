a341aaae1048a87d97774acf9b8f473526afe4a5
Sarang Bang <bangsarang7@gmail.com>
2021-11-22 21:04:51 +0530

    cccc

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ let weather = {
         console.log(visi_final);
         document.getElementById("name").innerHTML = name;
         document.getElementById("desp").innerText = "Description : " + description;
+        document.getElementById("icon").src="https://openweathermap.org/img/wn/"+ icon +".png";
         document.getElementById("temp").innerHTML = temp_final + "° C";
         document.getElementById("speed").innerText = "Wind Speed : " + speed + " km\hr";
         document.getElementById("visibility").innerText = "Visibility : " + visi_final + " km";
@@ -37,5 +38,5 @@ let weather = {
 
 function search() {
     weather.searchresult();
-    document.getElementById("city").value = null;
+   
 }
